ff6ce4faaed5a41391c5c38a52a3694d1d130f42
Cody Johnson <codyj@protonmail.com>
2019-07-13 13:53:02 -0400

    Add models

--- p1/src/main/java/models/Employee.java
+++ p1/src/main/java/models/Employee.java
@@ -1,4 +1,4 @@
-package beans;
+package models;
 
 public class Employee {
 	private int id;
--- p1/src/main/java/models/Reimbursement.java
+++ p1/src/main/java/models/Reimbursement.java
@@ -1,4 +1,4 @@
-package beans;
+package models;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
