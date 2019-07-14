2098530006c48b79caf68b17a71865a9e92f2a52
Cody Johnson <codyj@protonmail.com>
2019-07-14 19:15:17 -0400

    Create ConnectionService

---
+++ p1/src/main/java/services/ConnectionService.java
@@ -0,0 +1,48 @@
+package services;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionService {
+	private static ConnectionService instance;
+	private static Connection connection = null;
+
+	private ConnectionService() {
+		connection = makeConnection();
+	}
+
+	// Return singleton instance
+	public static synchronized ConnectionService getInstance() {
+		if (instance == null) {
+			instance = new ConnectionService();
+		}
+		return instance;
+	}
+
+	// Make connection
+	private Connection makeConnection() {
+		Connection c = null;
+		Properties p = new Properties();
+
+		try {
+			String propertiesFile = "database.properties";
+			p.load(new FileReader(propertiesFile));
+			Class.forName(p.getProperty("driver"));
+			c = DriverManager.getConnection(p.getProperty("url"),
+					p.getProperty("user"), p.getProperty("password"));
+		} catch (IOException | SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return c;
+	}
+
+	// Return connection
+	public Connection getConnection() {
+		return connection;
+	}
+}
