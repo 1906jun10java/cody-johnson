7fe3e18985d18daad73be8dbb3daccf091d0b5d7
Cody Johnson <codyj@protonmail.com>
2019-07-16 15:01:16 -0400

    Update LoginServlet

--- p1/src/main/java/servlets/LoginServlet.java
+++ p1/src/main/java/servlets/LoginServlet.java
@@ -1,19 +1,16 @@
 package servlets;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import daos.EmployeeDaoImpl;
-import models.Employee;
-import utilities.RequestUtility;
+import services.EmployeeService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
 
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = -6193286177757516310L;
+	private EmployeeService employeeService = EmployeeService.getInstance();
 
 	public LoginServlet() {}
 
@@ -23,30 +20,10 @@ public class LoginServlet extends HttpServlet {
 	}
 
 	// POST
-	protected void doPost(HttpServletRequest req, HttpServletResponse res) {
-		EmployeeDaoImpl edi = new EmployeeDaoImpl();
-		try {
-			PrintWriter pw = res.getWriter();
-			ObjectMapper om = new ObjectMapper();
-			String json = RequestUtility.bodyToJson(req);
-
-			Employee e1 = om.readValue(json, Employee.class);
-			Employee e2 = edi.getEmployee(e1.getEmail());
-
-			if (e2 == null) {
-				res.setStatus(401);
-				pw.write("{\"error\":" + "\"Invalid email or password\"}");
-				return;
-			}
-			if (e1.getPassword().equals(e2.getPassword())) {
-				res.setStatus(200);
-				pw.write("{\"id\":" + "\"" + e2.getId() + "\"}");
-			} else {
-				res.setStatus(401);
-				pw.write("{\"error\":" + "\"Invalid email or password\"}");
-			}
-		} catch (IOException | SQLException e) {
-			e.printStackTrace();
-		}
+	protected void doPost(HttpServletRequest req, HttpServletResponse res)
+	throws IOException {
+		PrintWriter pw = res.getWriter();
+		String jsonResponse = employeeService.login(req);
+		pw.write(jsonResponse);
 	}
 }
