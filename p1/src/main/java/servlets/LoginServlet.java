cf92ebd75bcbf7395d0d3110208a561f7da130c5
Cody Johnson <codyj@protonmail.com>
2019-07-16 19:34:21 -0400

    Update LoginServlet

--- p1/src/main/java/servlets/LoginServlet.java
+++ p1/src/main/java/servlets/LoginServlet.java
@@ -2,6 +2,7 @@ package servlets;
 
 import services.EmployeeService;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,19 +10,19 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = -6193286177757516310L;
 	private EmployeeService employeeService = EmployeeService.getInstance();
 
 	public LoginServlet() {}
 
 	// GET
-	protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+	protected void doGet(HttpServletRequest req, HttpServletResponse res)
+	throws IOException, ServletException {
 		System.out.println("GET");
 	}
 
 	// POST
 	protected void doPost(HttpServletRequest req, HttpServletResponse res)
-	throws IOException {
+	throws IOException, ServletException {
 		PrintWriter pw = res.getWriter();
 		String jsonResponse = employeeService.login(req);
 		pw.write(jsonResponse);
