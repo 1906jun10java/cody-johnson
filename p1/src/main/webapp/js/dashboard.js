8554c839e8429bd8980e207e79373b7e2d58f629
Cody Johnson <codyj@protonmail.com>
2019-07-16 20:20:35 -0400

    Update static files

--- p1/src/main/webapp/js/dashboard.js
+++ p1/src/main/webapp/js/dashboard.js
@@ -1,18 +1,31 @@
 window.onload = () => {
     validate();
+
+    // Event handlers
+    let logOutBtn = document.getElementById("logOutBtn").onclick = () => {
+        logOut();
+    };
 };
 
+// Validate user
 function validate() {
     let eId = sessionStorage.getItem("id");
-    if (!eId) {
-        window.location.replace("/login");
-    } else {
+    if (eId) {
         setHeading();
+    } else {
+        window.location.replace("/login");
     }
 }
 
+// Set welcome message in heading
 function setHeading() {
     let eFirstName = sessionStorage.getItem("firstName");
     let heading = document.getElementById("heading");
     heading.innerText = `Welcome, ${eFirstName}`;
+}
+
+// Clear session and forward to log in page
+function logOut() {
+    sessionStorage.clear();
+    window.location.replace("/login");
 }
