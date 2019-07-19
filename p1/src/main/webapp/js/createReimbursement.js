913af2c267b1070cef26d96943182519eb3f349b
Cody Johnson <codyj@protonmail.com>
2019-07-19 19:00:40 -0400

    Update form data to send index of reimbursement type select

--- p1/src/main/webapp/js/createReimbursement.js
+++ p1/src/main/webapp/js/createReimbursement.js
@@ -3,6 +3,15 @@ import {logOut, validate} from "./validation.js";
 // Check if user is valid
 validate();
 
+// For sending reimbursement type by selection index
+let reimbursementType = {
+	"Seminar":0,
+	"Preparation Classes":1,
+	"Certification":2,
+	"Technical Training":3,
+	"Other":4
+};
+
 window.onload = () => {
 	// Event handlers
 	document.getElementById("logOutBtn").onclick = () => {
@@ -18,8 +27,18 @@ let handleFormSubmit = () => {
 	const form = document.getElementById("reimbursementForm");
 	const file = document.querySelector('[type=file]').files;
 	let formData = new FormData(form);
+
+	// Use index of HTML select for reimbursement type
+	for (let entry of formData.entries()) {
+		if (entry[0] === "type") {
+			formData.set(entry[0], reimbursementType[entry[1]]);
+		}
+	}
+
+	// Append binary file to formData
 	formData.append("imgFile", file);
 
+	// POST to server
 	fetch("/reimbursement/create", {
 		method: "POST",
 		body: formData
