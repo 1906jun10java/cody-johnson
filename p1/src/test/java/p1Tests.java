248ad624c5bdc0642da7d647d2469f3cc5b77376
Cody Johnson <codyj@protonmail.com>
2019-07-25 13:23:00 -0400

    Add login() test

--- p1/src/test/java/p1Tests.java
+++ p1/src/test/java/p1Tests.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import daos.CredentialDaoImpl;
 import daos.EmployeeDaoImpl;
 import models.Credential;
@@ -5,15 +7,18 @@ import models.Employee;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import services.EmployeeService;
 
 import java.sql.SQLException;
 
 public class p1Tests {
+	private static EmployeeService es;
 	private static CredentialDaoImpl cdi;
 	private static EmployeeDaoImpl edi;
 
 	@BeforeClass
 	public static void beforeClass() {
+		es = EmployeeService.getInstance();
 		cdi = new CredentialDaoImpl();
 		edi = new EmployeeDaoImpl();
 	}
@@ -23,5 +28,17 @@ public class p1Tests {
 		Employee e = edi.getEmployee(45);
 		Credential c = cdi.getCredential(e.getEmail());
 		Assert.assertEquals(e.getId(), c.geteId());
+	}
+
+	@Test
+	public void testLogin() throws SQLException, JsonProcessingException {
+		Employee e = edi.getEmployee(45);
+		ObjectMapper om = new ObjectMapper();
+		String e1 = om.writeValueAsString(e);
+
+		Credential c = cdi.getCredential(e.getEmail());
+		String e2 = es.login(e.getEmail(), c.getPassword());
+
+		Assert.assertEquals(e1, e2);
 	}
 }
